ee793af12fc1ef953c619c3999f904e8a1251e4d
Pablohslima <pablohslima@gmail.com>
2024-07-16 18:56:57 -0300

    Começando a definir objtivos

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
-print('HELLO PEOPLE')
+"""
+# ============================= [ Informações ] ============================= #
+# Turn Meter Max: 100
+# Turn Meter Fill Rate: 0.07
+# Turn Meter to Turn: 100/0.07 = 1428.57143
+
+# =============================== [ Treino ] =============================== #
+[x] Criar Classe Hero
+    -> nome
+    -> vel
+    -> base_vel
+    -> barra
+[x] Criar Classe Skill
+[x] Criar Classe PreSet
+[x] Criar Classe Time
+
+
+"""
